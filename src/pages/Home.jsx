61adda13fdf564b2c9b75c055401de4c7a666a27
GerardoAJF <20240396@ricaldone.edu.sv>
2026-04-20 10:35:37 -0600

    Página DB creada

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <main className="container mx-auto flex h-screen flex-col items-center justify-center p-4 w-full">
+            <h1 className="text-center">Home</h1>
+            <p>Proyecto consumiento API's externas.</p>
+        </main>
+    );
+}
+
+export default Home;
